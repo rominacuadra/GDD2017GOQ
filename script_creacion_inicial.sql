91e7f8ca795f92d58197ce1ed7f0df98eb2f6bb2
rominacuadra <romicuadra@gmail.com>
2017-10-03 17:52:05 -0300

    mofificaciones varias

--- script_creacion_inicial.sql
+++ script_creacion_inicial.sql
@@ -22,8 +22,8 @@ create table GOQ.Funcionalidad(
 
 GO	
 create table GOQ.Funcionalidad_Rol(
-	fun_rol_rol_id int  not null, /* FK  REQUIRED.Rol */
-	fun_rol_fun_id int not null, /* FK  REQUIRED.Funcionalidad */
+	fun_rol_rol_id int  not null, /* FK  GOQ.Rol */
+	fun_rol_fun_id int not null, /* FK  GOQ.Funcionalidad */
 	CONSTRAINT PK_funcionalidad_rol PRIMARY KEY (fun_rol_rol_id, fun_rol_fun_id)
 ); 
 
@@ -32,28 +32,29 @@ create table GOQ.Usuario(
 	usu_id int CONSTRAINT PK_usu_id PRIMARY KEY NOT NULL IDENTITY(1,1),
 	usu_username varchar(25) not null UNIQUE,
 	usu_password varbinary(64) not null,
-	usu_bloqueado bit DEFAULT 0,
-	usu_intentos_Log int DEFAULT 0,
+	usu_intentos int DEFAULT 0,
+	usu_habilitado bit DEFAULT 0,
 );
 
 GO
 create table GOQ.Rol_Usuario(
-	rol_usu_rol_id int  not null,  /* FK  REQUIRED.Rol */
-	rol_usu_usu_id int  not null,  /* FK  REQUIRED.Usuario */
+	rol_usu_rol_id int  not null,  /* FK  GOQ.Rol */
+	rol_usu_usu_id int  not null,  /* FK  GOQ.Usuario */
 	CONSTRAINT PK_rol_usuario PRIMARY KEY (rol_usu_rol_id, rol_usu_usu_id)
-);GO
+);
+
 /******************************************** FIN - CREACION DE TABLAS *********************************************/
 
 /******************************************** INICIO - FOREING KEY *************************************************/
 GO
 alter table GOQ.Funcionalidad_Rol
-add constraint FK_fun_rol_rol_id foreign key (fun_rol_rol_id) references REQUIRED.Rol(rol_id),
-	constraint FK_fun_rol_fun_id foreign key (fun_rol_fun_id) references REQUIRED.Funcionalidad(fun_id);
+add constraint FK_fun_rol_rol_id foreign key (fun_rol_rol_id) references GOQ.Rol(rol_id),
+	constraint FK_fun_rol_fun_id foreign key (fun_rol_fun_id) references GOQ.Funcionalidad(fun_id);
 
 GO
 alter table GOQ.Rol_Usuario
-add constraint FK_rol_usu_rol_id foreign key (rol_usu_rol_id) references REQUIRED.Rol(rol_id),
-	constraint FK_rol_usu_usu_id foreign key (rol_usu_usu_id) references REQUIRED.Usuario(usu_id);
+add constraint FK_rol_usu_rol_id foreign key (rol_usu_rol_id) references GOQ.Rol(rol_id),
+	constraint FK_rol_usu_usu_id foreign key (rol_usu_usu_id) references GOQ.Usuario(usu_id);
 
 GO
 
