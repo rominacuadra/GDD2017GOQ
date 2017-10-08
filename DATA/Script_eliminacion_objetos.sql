50fee189384f2805d3daca3277fc10b151c4a96e
maria-angslz <maria.angslz@gmail.com>
2017-10-08 20:54:17 -0300

    poniendo en orden

---
+++ DATA/Script_eliminacion_objetos.sql
@@ -0,0 +1,21 @@
+USE [GD2C2017]
+GO
+
+/*******************************************
+***** ELIMINAR TABLAS ***************** 
+********************************************/
+
+
+DROP TABLE GOQ.Funcionalidad_Rol
+DROP TABLE GOQ.Funcionalidad
+DROP TABLE GOQ.Rol_Usuario
+DROP TABLE GOQ.Rol
+DROP TABLE GOQ.Usuario
+DROP FUNCTION GOQ.F_Hash256
+DROP FUNCTION GOQ.F_ObtenerPasswordGenerica
+
+/*******************************************
+***** ELIMINAR ESQUEMA ***************** 
+********************************************/
+
+DROP SCHEMA GOQ
