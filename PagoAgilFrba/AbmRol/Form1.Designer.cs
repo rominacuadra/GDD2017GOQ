cf6a44ee4d94340b9f43731ad4c259d60a9e40fb
rominasorayacañete <romysory@gmail.com>
2017-11-05 00:10:09 -0300

    Add files via upload
    versión final ABM de Rol

--- PagoAgilFrba/AbmRol/Form1.Designer.cs
+++ PagoAgilFrba/AbmRol/Form1.Designer.cs
@@ -29,12 +29,22 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbDevolucion_8 = new System.Windows.Forms.CheckBox();
+            this.cbEstadistico_9 = new System.Windows.Forms.CheckBox();
+            this.cbPagoRegistro_6 = new System.Windows.Forms.CheckBox();
+            this.cbPagoRendicion_7 = new System.Windows.Forms.CheckBox();
+            this.btnInhabilitar = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.cbSucursal_4 = new System.Windows.Forms.CheckBox();
+            this.cbFactura_5 = new System.Windows.Forms.CheckBox();
+            this.cbEmpresa_3 = new System.Windows.Forms.CheckBox();
+            this.cbRol_1 = new System.Windows.Forms.CheckBox();
+            this.cbCliente_2 = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.lbRoles = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.rolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.cerrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -44,21 +54,196 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.checkBox2);
-            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.btnModificar);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.cbDevolucion_8);
+            this.groupBox1.Controls.Add(this.cbEstadistico_9);
+            this.groupBox1.Controls.Add(this.cbPagoRegistro_6);
+            this.groupBox1.Controls.Add(this.cbPagoRendicion_7);
+            this.groupBox1.Controls.Add(this.btnInhabilitar);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.cbSucursal_4);
+            this.groupBox1.Controls.Add(this.cbFactura_5);
+            this.groupBox1.Controls.Add(this.cbEmpresa_3);
+            this.groupBox1.Controls.Add(this.cbRol_1);
+            this.groupBox1.Controls.Add(this.cbCliente_2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.lbRoles);
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(28, 38);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(576, 332);
+            this.groupBox1.Size = new System.Drawing.Size(789, 354);
             this.groupBox1.TabIndex = 7;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Administración de Roles";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnModificar.Location = new System.Drawing.Point(442, 315);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 9;
+            this.btnModificar.Text = "Aplicar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(190, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 25);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Funcionalidades";
+            // 
+            // cbDevolucion_8
+            // 
+            this.cbDevolucion_8.AutoSize = true;
+            this.cbDevolucion_8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbDevolucion_8.Location = new System.Drawing.Point(505, 219);
+            this.cbDevolucion_8.Name = "cbDevolucion_8";
+            this.cbDevolucion_8.Size = new System.Drawing.Size(105, 20);
+            this.cbDevolucion_8.TabIndex = 15;
+            this.cbDevolucion_8.Text = "Devolución";
+            this.cbDevolucion_8.UseVisualStyleBackColor = true;
+            // 
+            // cbEstadistico_9
+            // 
+            this.cbEstadistico_9.AutoSize = true;
+            this.cbEstadistico_9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbEstadistico_9.Location = new System.Drawing.Point(648, 219);
+            this.cbEstadistico_9.Name = "cbEstadistico_9";
+            this.cbEstadistico_9.Size = new System.Drawing.Size(132, 20);
+            this.cbEstadistico_9.TabIndex = 14;
+            this.cbEstadistico_9.Text = "List Estadistico";
+            this.cbEstadistico_9.UseVisualStyleBackColor = true;
+            // 
+            // cbPagoRegistro_6
+            // 
+            this.cbPagoRegistro_6.AutoSize = true;
+            this.cbPagoRegistro_6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPagoRegistro_6.Location = new System.Drawing.Point(196, 220);
+            this.cbPagoRegistro_6.Name = "cbPagoRegistro_6";
+            this.cbPagoRegistro_6.Size = new System.Drawing.Size(127, 20);
+            this.cbPagoRegistro_6.TabIndex = 13;
+            this.cbPagoRegistro_6.Text = "Pago Registro";
+            this.cbPagoRegistro_6.UseVisualStyleBackColor = true;
+            // 
+            // cbPagoRendicion_7
+            // 
+            this.cbPagoRendicion_7.AutoSize = true;
+            this.cbPagoRendicion_7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbPagoRendicion_7.Location = new System.Drawing.Point(342, 220);
+            this.cbPagoRendicion_7.Name = "cbPagoRendicion_7";
+            this.cbPagoRendicion_7.Size = new System.Drawing.Size(138, 20);
+            this.cbPagoRendicion_7.TabIndex = 12;
+            this.cbPagoRendicion_7.Text = "Pago Rendición";
+            this.cbPagoRendicion_7.UseVisualStyleBackColor = true;
+            // 
+            // btnInhabilitar
+            // 
+            this.btnInhabilitar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInhabilitar.Location = new System.Drawing.Point(109, 315);
+            this.btnInhabilitar.Name = "btnInhabilitar";
+            this.btnInhabilitar.Size = new System.Drawing.Size(75, 23);
+            this.btnInhabilitar.TabIndex = 11;
+            this.btnInhabilitar.Text = "Inhabilitar";
+            this.btnInhabilitar.UseVisualStyleBackColor = true;
+            this.btnInhabilitar.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(15, 315);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Crear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // cbSucursal_4
+            // 
+            this.cbSucursal_4.AutoSize = true;
+            this.cbSucursal_4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbSucursal_4.Location = new System.Drawing.Point(548, 155);
+            this.cbSucursal_4.Name = "cbSucursal_4";
+            this.cbSucursal_4.Size = new System.Drawing.Size(122, 20);
+            this.cbSucursal_4.TabIndex = 6;
+            this.cbSucursal_4.Text = "Adm Sucursal";
+            this.cbSucursal_4.UseVisualStyleBackColor = true;
+            // 
+            // cbFactura_5
+            // 
+            this.cbFactura_5.AutoSize = true;
+            this.cbFactura_5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbFactura_5.Location = new System.Drawing.Point(670, 155);
+            this.cbFactura_5.Name = "cbFactura_5";
+            this.cbFactura_5.Size = new System.Drawing.Size(114, 20);
+            this.cbFactura_5.TabIndex = 5;
+            this.cbFactura_5.Text = "Adm Factura";
+            this.cbFactura_5.UseVisualStyleBackColor = true;
+            // 
+            // cbEmpresa_3
+            // 
+            this.cbEmpresa_3.AutoSize = true;
+            this.cbEmpresa_3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbEmpresa_3.Location = new System.Drawing.Point(418, 155);
+            this.cbEmpresa_3.Name = "cbEmpresa_3";
+            this.cbEmpresa_3.Size = new System.Drawing.Size(124, 20);
+            this.cbEmpresa_3.TabIndex = 4;
+            this.cbEmpresa_3.Text = "Adm Empresa";
+            this.cbEmpresa_3.UseVisualStyleBackColor = true;
+            // 
+            // cbRol_1
+            // 
+            this.cbRol_1.AutoSize = true;
+            this.cbRol_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbRol_1.Location = new System.Drawing.Point(196, 155);
+            this.cbRol_1.Name = "cbRol_1";
+            this.cbRol_1.Size = new System.Drawing.Size(86, 20);
+            this.cbRol_1.TabIndex = 3;
+            this.cbRol_1.Text = "Adm Rol";
+            this.cbRol_1.UseVisualStyleBackColor = true;
+            // 
+            // cbCliente_2
+            // 
+            this.cbCliente_2.AutoSize = true;
+            this.cbCliente_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCliente_2.Location = new System.Drawing.Point(296, 155);
+            this.cbCliente_2.Name = "cbCliente_2";
+            this.cbCliente_2.Size = new System.Drawing.Size(110, 20);
+            this.cbCliente_2.TabIndex = 2;
+            this.cbCliente_2.Text = "Adm Cliente";
+            this.cbCliente_2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Roles";
+            // 
+            // lbRoles
+            // 
+            this.lbRoles.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRoles.FormattingEnabled = true;
+            this.lbRoles.ItemHeight = 16;
+            this.lbRoles.Location = new System.Drawing.Point(12, 81);
+            this.lbRoles.Name = "lbRoles";
+            this.lbRoles.Size = new System.Drawing.Size(173, 228);
+            this.lbRoles.TabIndex = 0;
+            this.lbRoles.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(320, 407);
+            this.button1.Location = new System.Drawing.Point(385, 414);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 8;
@@ -66,65 +251,13 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(200, 407);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Aplicar";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // listBox1
-            // 
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(12, 81);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(231, 228);
-            this.listBox1.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 49);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 16);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Seleccione el usuario:";
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox1.Location = new System.Drawing.Point(328, 214);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(116, 28);
-            this.checkBox1.TabIndex = 2;
-            this.checkBox1.Text = "Cobrador";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            // 
-            // checkBox2
-            // 
-            this.checkBox2.AutoSize = true;
-            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkBox2.Location = new System.Drawing.Point(328, 130);
-            this.checkBox2.Name = "checkBox2";
-            this.checkBox2.Size = new System.Drawing.Size(158, 28);
-            this.checkBox2.TabIndex = 3;
-            this.checkBox2.Text = "Administrador";
-            this.checkBox2.UseVisualStyleBackColor = true;
-            // 
             // menuStrip1
             // 
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.rolToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(632, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(830, 24);
             this.menuStrip1.TabIndex = 10;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -139,7 +272,7 @@
             // cerrarToolStripMenuItem
             // 
             this.cerrarToolStripMenuItem.Name = "cerrarToolStripMenuItem";
-            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cerrarToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
             this.cerrarToolStripMenuItem.Text = "Cerrar";
             this.cerrarToolStripMenuItem.Click += new System.EventHandler(this.cerrarToolStripMenuItem_Click);
             // 
@@ -147,8 +280,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(632, 452);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(830, 449);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
@@ -170,13 +302,23 @@
 
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.CheckBox checkBox2;
-        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.CheckBox cbRol_1;
+        private System.Windows.Forms.CheckBox cbCliente_2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lbRoles;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem rolToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem cerrarToolStripMenuItem;
+        private System.Windows.Forms.CheckBox cbEmpresa_3;
+        private System.Windows.Forms.CheckBox cbFactura_5;
+        private System.Windows.Forms.Button btnInhabilitar;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox cbSucursal_4;
+        private System.Windows.Forms.CheckBox cbEstadistico_9;
+        private System.Windows.Forms.CheckBox cbPagoRegistro_6;
+        private System.Windows.Forms.CheckBox cbPagoRendicion_7;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox cbDevolucion_8;
     }
 }
